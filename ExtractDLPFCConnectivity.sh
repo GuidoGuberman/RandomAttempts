3e39fe63d3651576e4808499cc5a971186a955ec
Guido Guberman <Guido@Ptito-Pittacus.local>
2018-05-01 21:54:22 -0400

    Added Extract and WBConn scripts

---
+++ ExtractDLPFCConnectivity.sh
@@ -0,0 +1,204 @@
+START_DIR=/Users/Guido/Documents/Analyses/subjects/PedsData/MNI
+
+cd $START_DIR
+
+mkdir ConnectivityProject
+grp1=(CC)
+grp2=(HC)
+grps=(CC HC)
+CC_grp=(TC001 TC007 TC033 TC042 TC054 TC056 TC057 TC071 TC073 TC074 TC076 TC077 TC078 TC079 TC050 TC069) #CC 
+HC_grp=(TC002 TC003 TC004 TC005 TC006 TC008 TC009 TC010 TC011 TC013 TC014 TC015 TC016 TC017 TC018 TC019 TC020 TC021 TC022 TC023 TC024 TC025 TC026 TC028 TC029 TC030 TC031 TC032 TC034 TC036 TC037 TC038 TC039 TC040 TC041 TC043 TC044 TC045 TC046 TC049 TC052 TC055 TC059 TC060 TC058 TC061) #HC 
+runs=(1)
+#parcs1=(Aicha_with_DLPFC)
+#parcs4=(Brainnetome_with_DLPFC)
+parcs2=(AICHA_1mm)
+parcs3=(BN_Atlas_246_1mm)
+#parcs=(Aicha_with_DLPFC Brainnetome_with_DLPFC AICHA_1mm BN_Atlas_246_1mm)
+parcs=(AICHA_1mm BN_Atlas_246_1mm)
+#parc1RNs=(1 21 22)
+parc2RNs=(1 20 21 22 23 24 25 26 27 28 29)
+parc3RNs=(1 16 17 20 21 22 23)
+#parc4RNs=(1 19 20)
+#####################################################################ALL VARIABLES ABOVE ARE MODIFIABLE#####################################################
+##############THE ONLY THING MODIFIABLE BELOW ARE SPECIFIC PATHS############################################################################################
+
+for grp in "${grp1[@]}"
+do
+	for nummni in "${CC_grp[@]}"
+	do
+		for numrun in "${runs[@]}"
+		do
+#			for parc in "${parcs1[@]}"
+#			do
+#				mkdir $START_DIR/ConnectivityProject/"$parc"
+#				cd $START_DIR/ConnectivityProject/"$parc"
+#				mkdir "$grp"_Group
+#			
+#				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+#				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#
+#				for parcRN in "${parc1RNs[@]}"
+#				do
+#					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#				done
+#				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+#			done
+
+#			for parc in "${parcs4[@]}"
+#			do
+#				mkdir $START_DIR/ConnectivityProject/"$parc"
+#				cd $START_DIR/ConnectivityProject/"$parc"
+#				mkdir "$grp"_Group
+#			
+#				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+#				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#
+#				for parcRN in "${parc4RNs[@]}"
+#				do
+#					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#				done
+#				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+#			done
+
+			for parc in "${parcs2[@]}"
+			do
+				mkdir $START_DIR/ConnectivityProject/"$parc"
+				cd $START_DIR/ConnectivityProject/"$parc"
+				mkdir "$grp"_Group
+
+				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+
+				for parcRN in "${parc2RNs[@]}"
+				do
+					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+				done
+				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+			done
+
+
+			for parc in "${parcs3[@]}"
+			do
+				mkdir $START_DIR/ConnectivityProject/"$parc"
+				cd $START_DIR/ConnectivityProject/"$parc"
+				mkdir "$grp"_Group
+
+				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+			
+				for parcRN in "${parc3RNs[@]}"
+				do
+					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+				done
+				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+			done
+		done
+	done
+done
+
+
+
+
+for grp in "${grp2[@]}"
+do
+	for nummni in "${HC_grp[@]}"
+	do
+		for numrun in "${runs[@]}"
+		do
+#			for parc in "${parcs1[@]}"
+#			do
+#				mkdir $START_DIR/ConnectivityProject/"$parc"
+#				cd $START_DIR/ConnectivityProject/"$parc"
+#				mkdir "$grp"_Group
+#			
+#				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+#				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#
+#				for parcRN in "${parc1RNs[@]}"
+#				do
+#					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#				done
+#				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+#			done
+#
+#			for parc in "${parcs4[@]}"
+#			do
+#				mkdir $START_DIR/ConnectivityProject/"$parc"
+#				cd $START_DIR/ConnectivityProject/"$parc"
+#				mkdir "$grp"_Group
+#			
+#				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+#				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#
+#				for parcRN in "${parc4RNs[@]}"
+#				do
+#					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+#				done
+#				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+#				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+#			done
+
+
+			for parc in "${parcs2[@]}"
+			do
+				mkdir $START_DIR/ConnectivityProject/"$parc"
+				cd $START_DIR/ConnectivityProject/"$parc"
+				mkdir "$grp"_Group
+
+				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+
+				for parcRN in "${parc2RNs[@]}"
+				do
+					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+				done
+				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+			done
+
+
+			for parc in "${parcs3[@]}"
+			do
+				mkdir $START_DIR/ConnectivityProject/"$parc"
+				cd $START_DIR/ConnectivityProject/"$parc"
+				mkdir "$grp"_Group
+
+				cd $START_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"/"$nummni"_"$numrun"_ROIs/"$nummni"_"$numrun"_ConnectivityProject
+				rm "$nummni"_run"$numrun"_"$parc"_spConn.txt
+			
+				for parcRN in "${parc3RNs[@]}"
+				do
+					rm "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+					echo "$nummni" `awk "NR==$parcRN {print}" "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv` > "$nummni"_run"$numrun"_"$parc"_spConntmp"$parcRN".txt
+				done
+				cat "$nummni"_run"$numrun"_"$parc"_spConntmp*.txt > "$nummni"_run"$numrun"_"$parc"_spConn.txt
+				cp "$nummni"_run"$numrun"_"$parc"_spConn.txt $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/"$nummni"_run"$numrun"_"$parc"_spConn.txt
+			done
+		done
+	done
+done
+
+for numrun in "${runs[@]}"
+do
+	for parc in "${parcs[@]}"
+	do
+		for grp in "${grps[@]}"
+		do
+			cd $START_DIR/ConnectivityProject/"$parc"/"$grp"_Group/
+			rm ALL_"$grp"_run"$numrun"_"$parc"_spConn.txt
+			cat *_run"$numrun"_"$parc"_spConn.txt > ALL_"$grp"_run"$numrun"_"$parc"_spConn.txt
+		done
+	done
+done
---
+++ WBConnectivity.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --account=def-aptito
+#SBATCH --time=20:00:00
+#SBATCH --mem-per-cpu=4028M
+#SBATCH --mail-user=guidoguberman@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+TEMPLATE_DIR=/Users/Guido/Documents/Analyses/subjects/PedsData/Templates
+ROI_DIR=/Users/Guido/Documents/Analyses/subjects/PedsData/Templates/DLPFCConnectivityStudy
+WORKING_DIR=/Users/Guido/Documents/Analyses/subjects/PedsData/MNI
+npv=24
+seeding_method=int
+subs=(TC069 TC061)
+#(TC001 TC002 TC003 TC004 TC005 TC006 TC007 TC008 TC009 TC010 TC011 TC013 TC014 TC015 TC016 TC017 TC018 \
+#	TC019 TC020 TC021 TC022 TC023 TC024 TC025 TC026 TC028 TC029 TC030 TC031 TC032 TC033 TC034 TC036 TC037 TC038 TC039 TC040 TC041 TC042 TC043 TC044 TC045 TC046 TC049 TC052 \
+#	TC054 TC055 TC056 TC057 TC059 TC060 TC071 TC073 TC074 TC076 TC077 TC078 TC079 TC050 TC058 TC069 TC061)
+runs=(1)
+parcs=(AICHA_1mm BN_Atlas_246_1mm)
+
+######################################################
+for nummni in "${subs[@]}" 
+do
+	for numrun in "${runs[@]}"
+	do
+
+		cd $WORKING_DIR/"$nummni"/DWI/Preproc1/"$nummni"_diff"$numrun"
+#
+
+		cd "$nummni"_"$numrun"_ROIs
+#		echo "Registering MNI to DWI space, suject $nummni run $numrun"
+#		rm "$nummni"_MNI2DWI"$numrun"_0GenericAffine.mat "$nummni"_MNI2DWI"$numrun"_test1InverseWarp.nii.gz "$nummni"_MNI2DWI"$numrun"_Warp.nii.gz "$nummni"_MNI2DWI"$numrun"_Warped.nii.gz
+#		antsRegistrationSyN.sh -d 3 -f ../fa.nii.gz -f ../../"$nummni"_b0c_"$numrun"_1x1x1.nii -m $TEMPLATE_DIR/MNI152_T1_1mm_brain.nii -m $TEMPLATE_DIR/MNI152_T1_1mm_brain.nii -o "$nummni"_MNI2DWI"$numrun"_
+
+#		mkdir "$nummni"_"$numrun"_ConnectivityProject
+		cd "$nummni"_"$numrun"_ConnectivityProject
+	
+		for parc in "${parcs[@]}"
+		do
+			echo "Transforming parcellation $parc to native DWI space, subject $nummni run $numrun"
+			rm "$nummni"_"$parc"_in_dwi"$numrun".nii
+			antsApplyTransforms -d 3 -r ../../../"$nummni"_b0c_"$numrun"_1x1x1.nii -t [../"$nummni"_MNI2DWI"$numrun"_0GenericAffine.mat,0] -n MultiLabel -i $ROI_DIR/"$parc".nii* -o "$nummni"_"$parc"_in_dwi"$numrun".nii -v 1
+
+			echo "Computing whole-rain connectivity for subject $nummni run $numrun based on parcellation $parc"
+			scil_compute_robust_connectivity_matrix.py ../../"$nummni"_run"$numrun"_prob_pft_fodf_npv"$npv"_"$seeding_method"_fc02_20-200_noloops_migrated.trk "$nummni"_"$parc"_in_dwi"$numrun".nii "$nummni"_run"$numrun"_"$parc"_ConnMat_norm.csv --no_self_connection --normalize --tp trackvis -f
+			scil_compute_robust_connectivity_matrix.py ../../"$nummni"_run"$numrun"_prob_pft_fodf_npv"$npv"_"$seeding_method"_fc02_20-200_noloops_migrated.trk "$nummni"_"$parc"_in_dwi"$numrun".nii "$nummni"_run"$numrun"_"$parc"_ConnMat.csv --no_self_connection --tp trackvis -f
+
+		done
+	done
+done
+
