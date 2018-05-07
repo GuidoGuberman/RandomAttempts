48c29e5c8fa65e553a7f886a3701eaa6702b8da3
Guido Guberman <guidoguberman@gmail.com>
2018-05-07 11:19:01 -0500

    Small edit.

--- ExtractTractometryData.sh
+++ ExtractTractometryData.sh
@@ -2,7 +2,7 @@ CC_grp=(TC001)
 #(TC001 TC007 TC033 TC042 TC054 TC056 TC057 TC071 TC073 TC074 TC076 TC077 TC078 TC079 TC050 TC069) #CC
 HC_grp=(TC002)
 #(TC002 TC003 TC004 TC005 TC006 TC008 TC009 TC010 TC011 TC013 TC014 TC015 TC016 TC017 TC018 TC019 TC020 TC021 TC022 TC023 TC024 TC025 TC026 TC028 TC029 TC030 TC031 TC032 TC034 TC036 \
-TC037 TC038 TC039 TC040 TC041 TC043 TC044 TC045 TC046 TC049 TC052 TC055 TC059 TC060 TC058 TC061) #HC
+#TC037 TC038 TC039 TC040 TC041 TC043 TC044 TC045 TC046 TC049 TC052 TC055 TC059 TC060 TC058 TC061) #HC
 runs=(1)
 START_DIR=/Users/Guido/Documents/Analyses/subjects/PedsData/MNI
 OUT_DIR=$START_DIR/ConnectivityProject/
@@ -51,12 +51,12 @@ do
     done
   done
         #AFD along streamlines
-        awk "/mean_afd/{print}" "$nummni"_run"$numrun"_"$parc"_AFDalongstrs.txt >>tmpAFD.txt
-        awk -F ":|," '{print $2}' tmpAFD.txt > tmpAFD.txt
+        echo `awk "/mean_afd/{print}" "$nummni"_run"$numrun"_"$parc"_AFDalongstrs.txt` >>$OUT_DIR/"$parc"/"$GRP"_Group/tmpAFD.txt
+        echo `awk -F ":|," '{print $2}' tmpAFD.txt` > $OUT_DIR/"$parc"/"$GRP"_Group/tmpAFD.txt
         paste tmp2.txt tmpAFD.txt > $OUT_DIR/"$parc"/"$GRP"_Group/All_"$GRP"_run"$numrun"_"$parc"_AFDalongstrs.txt
 
         #DTI metrics
-        echo awk '{print $2 " " $3 " " $4 " " $5 " " $6}' All_"$GRP"_run"$numrun"_"$parc"_FA.txt
+        echo `awk '{print $2 " " $3 " " $4 " " $5 " " $6}' `> All_"$GRP"_run"$numrun"_"$parc"_FA.txt
         awk '{print $2 " " $3 " " $7 " " $8 " " $9}' All_"$GRP"_run"$numrun"_"$parc"_MD.txt
         awk '{print $2 " " $3 " " $10 " " $11 " " $12}' All_"$GRP"_run"$numrun"_"$parc"_RD.txt
         awk '{print $2 " " $3 " " $13 " " $14 " " $15}' All_"$GRP"_run"$numrun"_"$parc"_AD.txt
